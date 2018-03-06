e8da4caebd6525be632acd64e503fd0ed4d57c13
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-03-06 08:15:40 -0300

    inicio do projeto

---
+++ src/ByteBank.Forum/Controllers/ContaController.cs
@@ -0,0 +1,112 @@
+﻿using ByteBank.Forum.Models;
+using ByteBank.Forum.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ByteBank.Forum.Controllers
+{
+    public class ContaController : Controller
+    {
+
+        private UserManager<UsuarioAplicacao> _userManager;
+        public UserManager<UsuarioAplicacao> UserManager
+        {
+            get
+            {
+                if(_userManager == null)
+                {
+                    var contextOwin = HttpContext.GetOwinContext();
+                    _userManager = contextOwin.GetUserManager<UserManager<UsuarioAplicacao>>();
+                }
+                return _userManager;
+            }
+            set
+            {
+                _userManager = value;
+            }
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Registrar(ContaRegistrarViewModel modelo)
+        {
+            if(ModelState.IsValid)
+            {
+                var novoUsuario = new UsuarioAplicacao();
+
+                novoUsuario.Email = modelo.Email;
+                novoUsuario.UserName = modelo.UserName;
+                novoUsuario.NomeCompleto = modelo.NomeCompleto;
+
+                var usuario = await UserManager.FindByEmailAsync(modelo.Email);
+                var usuarioJaExiste = usuario != null;
+
+                if (usuarioJaExiste)
+                    return View("AguardandoConfirmacao");
+
+                var resultado = await UserManager.CreateAsync(novoUsuario, modelo.Senha);
+
+                if (resultado.Succeeded)
+                {
+                    // Enviar o email de confirmação
+                    await EnviarEmailDeConfirmacaoAsync(novoUsuario);
+                    return View("AguardandoConfirmacao");
+                }
+                else
+                {
+                    AdicionaErros(resultado);
+                }
+            }
+
+            // Alguma coisa de errado aconteceu!
+            return View(modelo);
+        }
+
+        private async Task EnviarEmailDeConfirmacaoAsync(UsuarioAplicacao usuario)
+        {
+            var token = await UserManager.GenerateEmailConfirmationTokenAsync(usuario.Id);
+
+            var linkDeCallback =
+                Url.Action(
+                    "ConfirmacaoEmail",
+                    "Conta",
+                    new { usuarioId = usuario.Id, token = token },
+                    Request.Url.Scheme);
+
+            await UserManager.SendEmailAsync(
+                usuario.Id,
+                "Fórum ByteBank - Confirmação de Email",
+                $"Bem vindo ao fórum ByteBank, clique aqui {linkDeCallback} para confirmar seu email!");
+        }
+
+        public async Task<ActionResult> ConfirmacaoEmail(string usuarioId, string token)
+        {
+            if (usuarioId == null || token == null)
+                return View("Error");
+
+            var resultado = await UserManager.ConfirmEmailAsync(usuarioId, token);
+
+            if (resultado.Succeeded)
+                return RedirectToAction("Index", "Home");
+            else
+                return View("Error");
+        }
+        
+        private void AdicionaErros(IdentityResult resultado)
+        {
+            foreach (var erro in resultado.Errors)
+                ModelState.AddModelError("", erro);
+        }
+    }
+}
---
+++ src/ByteBank.Forum/Startup.cs
@@ -0,0 +1,62 @@
+﻿using ByteBank.Forum.App_Start.Identity;
+using ByteBank.Forum.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(ByteBank.Forum.Startup))]
+
+namespace ByteBank.Forum
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.CreatePerOwinContext<DbContext>(() =>
+                new IdentityDbContext<UsuarioAplicacao>("DefaultConnection"));
+
+            builder.CreatePerOwinContext<IUserStore<UsuarioAplicacao>>(
+                (opcoes, contextoOwin) =>
+                {
+                    var dbContext = contextoOwin.Get<DbContext>();
+                    return new UserStore<UsuarioAplicacao>(dbContext);
+                });
+
+            builder.CreatePerOwinContext<UserManager<UsuarioAplicacao>>(
+                (opcoes, contextoOwin) =>
+                {
+                    var userStore = contextoOwin.Get<IUserStore<UsuarioAplicacao>>();
+                    var userManager = new UserManager<UsuarioAplicacao>(userStore);
+
+                    var userValidator = new UserValidator<UsuarioAplicacao>(userManager);
+                    userValidator.RequireUniqueEmail = true;
+
+                    userManager.UserValidator = userValidator;
+                    userManager.PasswordValidator = new SenhaValidador()
+                    {
+                        TamanhoRequerido = 6,
+                        ObrigatorioCaracteresEspeciais = true,
+                        ObrigatorioDigitos = true,
+                        ObrigatorioLowerCase = true,
+                        ObrigatorioUpperCase = true
+                    };
+
+                    userManager.EmailService = new EmailServico();
+
+                    var dataProtectionProvider = opcoes.DataProtectionProvider;
+                    var dataProtectionProviderCreated = dataProtectionProvider.Create("ByteBank.Forum");
+
+                    userManager.UserTokenProvider = new DataProtectorTokenProvider<UsuarioAplicacao>(dataProtectionProviderCreated);
+
+                    return userManager;
+                });
+        }
+    }
+}
